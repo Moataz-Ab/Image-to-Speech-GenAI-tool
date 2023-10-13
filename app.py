14cfca338d9a901eadcd339c8d6f176dc9a12778
Moataz Abdelmegid <moataz.abdelmegid@outlook.com>
2023-10-13 15:09:32 +0200

    notebook updated

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ def image_to_text(image_url):
 
 #llm
 def text_to_story(text, model='gpt-3.5-turbo-instruct-0914', temperature=0.9):
-    llm = OpenAI(openai_api_key=OPENAI_KEY, model=model, temperature=temperature))
+    llm = OpenAI(openai_api_key=OPENAI_KEY, model=model, temperature=temperature)
     prompt = PromptTemplate(
         input_variables = ['text'],
         template = '''
         You are a talented story teller who can create a story from a simple narrative./
         Create a story using the following scenario; the story should have be maximum 20 words long.
-        context = {scenario}
+        context = {text}
         '''
         )
     chain = LLMChain(llm=llm, prompt=prompt)
